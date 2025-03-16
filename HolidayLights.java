e0ea69262d1f9d812e78aac01e7bc9759a9962e3
ChayaTheCoder <mmoham43@charlotte.edu>
2025-03-16 19:44:19 -0400

    Update HolidayLights.java

--- HolidayLights.java
+++ HolidayLights.java
@@ -11,8 +11,8 @@
  * - Remove a light of a specific color.  
  * - Replace all occurrences of a color with another color.  
  * 
- * @ Michaya Mohamed
- * @ Version 3-16-25
+ * @Author: Michaya Mohamed
+ * @Version: 3-16-25
  */
 public class HolidayLights {
     public static void main(String argv[]) {
